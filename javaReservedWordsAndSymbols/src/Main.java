2191e323749e5b6d5dba156f6a7aa8f02d5421f6
Aditpa <124455792+Aditpa@users.noreply.github.com>
2023-08-17 15:15:11 +0200

    Add files via upload

---
+++ javaReservedWordsAndSymbols/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+//        valid
+        char a;
+        int b;
+        String neno;
+        if;
+        while;
+
+//        invalid
+        whileing;
+        charing;
+        string;
+        Int;
+        DoUBle;
+    }
+}
